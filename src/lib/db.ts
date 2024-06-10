dcb02a037485bbe98c781722c99b57250ce082c2
Carlos Manuel García Bello <carloscrackcm4@gmail.com>
2024-06-10 11:29:46 +0200

    Database vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,13 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'cocina',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections:true,
   connectionLimit: 10, 
+  queueLimit:0
 });
 
 export default pool;
