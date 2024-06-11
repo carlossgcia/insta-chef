a55b77689a6bb0ab3348503baf57105666681496
Carlos Manuel García Bello <carloscrackcm4@gmail.com>
2024-06-11 13:04:24 +0200

    DAta bases

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host:"localhost",
-  user: "root",
-  password: "",
-  database: "cocina2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
   waitForConnections:true,
   connectionLimit: 10, 
   queueLimit:0
